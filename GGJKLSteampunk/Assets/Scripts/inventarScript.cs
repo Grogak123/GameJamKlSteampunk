935ae0f840c1d317f7469768f9434b672d8550d2
Willy <w_loedts14@cs.uni-kl.de>
2020-01-31 21:18:19 +0100

    Prefabs für Items & einfaches itemAttachementScript eingefügt

---
+++ GGJKLSteampunk/Assets/Scripts/inventarScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class inventarScript : MonoBehaviour
+{
+    
+    private sbyte scrap, feather, screw, gear, pump, clockwork, nucReactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrap = 0;
+        feather = 0;
+        screw = 0;
+        gear = 0;
+        pump = 0;
+        clockwork = 0;
+        nucReactor = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void itemCollected (string name)
+    {
+        switch(name)
+        {
+            case "Scrap":
+                scrap++;
+                break;
+            case "Feather":
+                feather++;
+                break;
+            case "Screw":
+                screw++;
+                break;
+            case "Gear":
+                gear++;
+                break;
+            default:
+                Debug.Log("Falscher Wert bei itemCollected im inventarScript übergeben: " + name);
+                break;
+        }
+
+
+
+    }
+}
---
+++ GGJKLSteampunk/Assets/Scripts/itemAttachScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class itemAttachScript : MonoBehaviour
+{
+    private string itemName;
+    private inventarScript invScript;
+    GameObject mainChar;
+
+    void Start()
+    {
+        mainChar = GameObject.Find("MainCharacter");
+        itemName = gameObject.tag;
+        invScript = (inventarScript)mainChar.GetComponent(typeof(inventarScript));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        invScript.itemCollected(itemName);
+        Destroy(gameObject);
+
+    }
+}
