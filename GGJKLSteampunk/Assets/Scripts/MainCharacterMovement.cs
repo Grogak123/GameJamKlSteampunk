503359db210e24db1929bccda72e6b6a74797be3
Worlz <GameJam@Tychus.uni-kl.de>
2020-01-31 20:14:00 +0100

    test2

--- GGJKLSteampunk/Assets/Scripts/MainCharacterMovement.cs
+++ GGJKLSteampunk/Assets/Scripts/MainCharacterMovement.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCharacterMovement : MonoBehaviour
+namespace gamejam
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MainCharacterMovement : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
