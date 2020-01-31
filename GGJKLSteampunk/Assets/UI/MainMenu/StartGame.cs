1d96b03ee05cec0abff577e74994432ad87044cc
Worlz <GameJam@Tychus.uni-kl.de>
2020-01-31 21:20:06 +0100

    commit start game

---
+++ GGJKLSteampunk/Assets/UI/MainMenu/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public string GameSceneName;
+    public void LoadGameScene()
+        {
+
+            if (GameSceneName != null)
+            {
+                SceneManager.LoadScene(GameSceneName);
+            }
+            else
+            {
+                Debug.Log("Please set the String for scene Name");
+            }
+        }
+}
