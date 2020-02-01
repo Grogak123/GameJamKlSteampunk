5efae9c8d0709650a3d16acf1090184da2f1b784
Worlz <GameJam@Tychus.uni-kl.de>
2020-02-01 22:00:10 +0100

    timerprogress

---
+++ GGJKLSteampunk/Assets/Scripts/Clock.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Clock : MonoBehaviour
+{
+
+
+
+    public RectTransform pointer;
+
+
+    float referenceTime;
+
+    
+    public float seconds;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        referenceTime += Time.deltaTime;
+        
+        Debug.Log("refTime: " + Mathf.Round(referenceTime));
+        pointer.rotation = Quaternion.Euler(0, 0, referenceTime / 360);
+        Debug.Log(pointer.rotation);
+
+        if(referenceTime > seconds)
+        {
+            Debug.Log("LOSER");
+        }
+
+    }
+
+}
