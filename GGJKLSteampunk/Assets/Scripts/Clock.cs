c449ec8a340832a4eb942cb0bafe31e5e28d0853
Worlz <GameJam@Tychus.uni-kl.de>
2020-02-01 22:20:49 +0100

    timer push

--- GGJKLSteampunk/Assets/Scripts/Clock.cs
+++ GGJKLSteampunk/Assets/Scripts/Clock.cs
@@ -11,18 +11,27 @@ public class Clock : MonoBehaviour
 
 
     float referenceTime;
-
+    float test;
     
     public float seconds;
 
+    float elapsed = 0f;
 
     // Update is called once per frame
     void Update()
     {
+
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
+        {
+            elapsed = elapsed % 1f;
+            OutputTime();
+        }
+
         referenceTime += Time.deltaTime;
         
         Debug.Log("refTime: " + Mathf.Round(referenceTime));
-        pointer.rotation = Quaternion.Euler(0, 0, referenceTime / 360);
+        pointer.rotation = Quaternion.Euler(0, 0, -test);
         Debug.Log(pointer.rotation);
 
         if(referenceTime > seconds)
@@ -30,6 +39,11 @@ public class Clock : MonoBehaviour
             Debug.Log("LOSER");
         }
 
+    }
+
+    void OutputTime()
+    {
+        test += (360 / seconds);
     }
 
 }
