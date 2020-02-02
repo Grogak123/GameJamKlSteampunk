953215a9ecffe6646dd6b1f7434d8864e51ec4e7
Willy <w_loedts14@cs.uni-kl.de>
2020-02-02 15:26:13 +0100

    Animationen für Screens

---
+++ GGJKLSteampunk/Assets/Scripts/CreditsFolder/characterCredits.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterCredits : MonoBehaviour
+{
+    Vector3 position;
+    Animator animator;
+    public float timeUntilArrived = 1.5f;
+    bool isAttacking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = gameObject.transform.position;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(!isAttacking)
+        {
+            isAttacking = true;
+            StartCoroutine(attack());
+        }
+        
+    }
+
+    IEnumerator attack()
+    {
+
+        animator.SetFloat("Speed", 1);
+        yield return new WaitForSeconds(1);
+
+
+        Vector3 targetPosition = new Vector3(-14, -2.42f, 0);
+        
+        float speed = Vector3.Distance(targetPosition, position) / timeUntilArrived;
+
+
+        /*
+         * callt den animator und lässt so die entsprechende animation laufen
+         */
+
+
+                animator.SetFloat("Horizontal", -1);
+
+        animator.SetFloat("speed", 1);
+        while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1);
+        speed = Vector3.Distance(transform.position, position) / timeUntilArrived;
+
+        animator.SetFloat("Horizontal", 1);
+        while (Vector3.Distance(transform.position, position) > 0.1f)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, position, speed * Time.deltaTime);
+            yield return null;
+        }
+        isAttacking = false;
+        animator.SetFloat("speed", 0);
+
+
+    }
+
+}
