80c2163f495783d21f818e71bb7c5dbc3da2430f
Worlz <GameJam@Tychus.uni-kl.de>
2020-02-02 14:01:06 +0100

    ztz

---
+++ GGJKLSteampunk/Assets/BackgroundMusicScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BackgroundMusicScript : MonoBehaviour
+{
+
+    public AudioSource StartClip;
+    public AudioSource LoopClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartClip.Play();
+        LoopClip.PlayDelayed(11.0f);
+    }
+
+
+}
